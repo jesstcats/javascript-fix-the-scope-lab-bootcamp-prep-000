dc966dcaed43fc6c066c1ad50fb4c102453aefa1
jessica cats <jesstcats@gmail.com>
2019-04-10 21:16:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
-var animal= 'dog'
-
-function myAninal () {
+var animal = 'dog'
+ 
+function myAnimal() {
   return animal
 }
-
-function yourAninal () {
-  var animal = 'cat';
+ 
+function yourAnimal() {
+  var animal = 'cat'
+  
   return animal
 }
-
-function add2(n) {
-  const two=2 
-  return n + two
-}
-
+ 
+ function add2(n) {
+  const two = 2
+  return parseInt(n, 10) + two
+ 
+  
 var funkyFunction = function() {
   return function() {
     return "FUNKY!"
   }
 }
-var theFunk = funkyFunction()
-
-theFunk();
